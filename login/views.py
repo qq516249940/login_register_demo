dc1010f515378f17ad687c324a110ccbca86ba00
yancong <516249940@qq.com>
2022-11-28 11:59:29 +0800

    验证用户名和密码

--- login/views.py
+++ login/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
+from . import models
+
+
 # Create your views here.
 
 
@@ -10,7 +13,19 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username.strip() and password:  # 确保用户名和密码都不为空
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                return render(request, 'login/login.html')
+            if user.password == password:
+                return redirect('/index/')
     return render(request, 'login/login.html')
 
 
