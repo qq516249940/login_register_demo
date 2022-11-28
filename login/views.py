8ff0ab07fb4590a832286d00878ee25812fdb5ec
yancong <516249940@qq.com>
2022-11-28 14:21:22 +0800

    登陆添加添加提示信息

--- login/views.py
+++ login/views.py
@@ -13,19 +13,28 @@ def index(request):
 
 
 def login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        if username.strip() and password:  # 确保用户名和密码都不为空
+        message = '请检查填写的内容！'
+        if username.strip() and password:
             # 用户名字符合法性验证
             # 密码长度验证
             # 更多的其它验证.....
             try:
                 user = models.User.objects.get(name=username)
-            except:
-                return render(request, 'login/login.html')
+            except :
+                message = '用户不存在！'
+                return render(request, 'login/login.html', {'message': message})
+
             if user.password == password:
+                print(username, password)
                 return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
